6d9cc8a2c8fd9fed894dec193ed1c518a4a68878
Cao007 <1114685490@qq.com>
2024-06-12 21:50:51 +0800

    新增giscus评论系统

--- .vitepress/theme/components/Comment.vue
+++ .vitepress/theme/components/Comment.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>
-        <h3>评论</h3>
+    <div style="margin-top: 24px">
+        <Giscus
+            id="comments"
+            repo="Cao007/blog"
+            repoid="R_kgDOMFRFTg"
+            category="General"
+            categoryid="DIC_kwDOMFRFTs4CgCRv"
+            mapping="pathname"
+            term="Welcome to giscus!"
+            reactionsenabled="1"
+            emitmetadata="0"
+            inputposition="top"
+            loading="lazy"
+            :theme="isDark ? 'dark' : 'light'"
+            :key="route.path"
+            lang="zh-CN"
+        ></Giscus>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import Giscus from '@giscus/vue';
+import { useRoute, useData } from 'vitepress';
 
-
+const route = useRoute();
+const { isDark } = useData();
 </script>
-
-<style scoped></style>
